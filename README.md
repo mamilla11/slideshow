9b97ad2603290cc90e58567994e5636ff9374356
Anastasiia Lazareva <nastiia.lazareva@gmail.com>
2024-02-23 15:36:09 +0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # slideshow
+
+### Create virtual environment:
+
+```bash
+python -m venv venv
+```
+```bash
+venv/Scripts/activate
+```
+
+### Install dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+### Run application:
+
+```bash
+python app/main.py
+```
---
+++ app/main.py
@@ -0,0 +1,74 @@
+import glob
+import os
+
+import pygame
+from pygame.locals import *
+import serial
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+IMAGE_FOLDER = 'app/Samples'
+BAUDRATE = int(os.environ.get('BAUDRATE', 9600))
+SCALE_FACTOR = float(os.environ.get('SCALE_FACTOR', 0.5))
+
+
+# Initialize image viewer
+pygame.init()
+infoObject = pygame.display.Info()
+screen = pygame.display.set_mode((infoObject.current_w, infoObject.current_h), pygame.FULLSCREEN)
+pygame.display.set_caption('Image Viewer')
+pygame.mouse.set_visible(False)
+
+
+def list_images(folder):
+    return glob.glob(os.path.join(folder, '*.jpg')) + glob.glob(os.path.join(folder, '*.png'))
+
+
+def display_image(image_path):
+    # Load the image
+    original_image = pygame.image.load(image_path)
+
+    # Calculate the scale factor to fit the image on the screen
+    screen_width, screen_height = pygame.display.get_surface().get_size()
+    image_width, image_height = original_image.get_size()
+
+    # Scale the image
+    scaled_image = pygame.transform.scale(original_image, (int(image_width * SCALE_FACTOR), int(image_height * SCALE_FACTOR)))
+
+    # Calculate the position to center the image
+    x = (screen_width - scaled_image.get_width()) // 2
+    y = (screen_height - scaled_image.get_height()) // 2
+
+    screen.blit(scaled_image, (x, y))
+    pygame.display.flip()
+
+
+def quit():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                return True
+    return False
+
+
+def run():
+    image_files = list_images(IMAGE_FOLDER)
+    current_image_index = 0
+
+    clock = pygame.time.Clock()
+    while not quit():
+        current_image_index = (current_image_index + 1) % len(image_files)
+        display_image(image_files[current_image_index])
+        pygame.display.update()
+        clock.tick(1)
+
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    run()
